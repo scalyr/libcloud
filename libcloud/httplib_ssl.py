3938c98974f38bc2092c2588f50cbf77264f3237
Tomaž Muraus <kami@k5-storitve.net>
2010-11-16 01:19:23 +0100

    Change license header and fix the issue with alt_names being None.

--- libcloud/httplib_ssl.py
+++ libcloud/httplib_ssl.py
@@ -1,30 +1,17 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2010, Tomaž Muraus
-# All rights reserved.
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of the Tomaž Muraus nor the
-#       names of its contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# Based on example from post "HTTPS Certificate Verification in Python With urllib2" -
-# http://www.muchtooscrawled.com/2010/03/https-certificate-verification-in-python-with-urllib2/
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import socket
 import ssl
@@ -61,7 +48,7 @@ class VerifiedHTTPSConnection(httplib.HTTPSConnection):
 
     def _get_subjectAltName(self, cert):
         if not cert.has_key('subjectAltName'):
-            return None
+            return []
 
         alt_names = []
         for value in cert['subjectAltName']:
@@ -77,4 +64,5 @@ class VerifiedHTTPSConnection(httplib.HTTPSConnection):
         for value in cert['subject']:
             if value[0][0].lower() == 'commonname':
                 return value[0][1]
+
         return None
