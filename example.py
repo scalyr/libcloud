3f21d6b43bebffbb31513c148cf97dac6b8091cd
Tomaz Muraus <tomaz@tomaz.me>
2011-02-27 20:37:20 +0100

    Update paths in example.py

--- example.py
+++ example.py
@@ -12,8 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from libcloud.types import Provider
-from libcloud.providers import get_driver
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
 
 EC2 = get_driver(Provider.EC2_US_EAST)
 Slicehost = get_driver(Provider.SLICEHOST)
