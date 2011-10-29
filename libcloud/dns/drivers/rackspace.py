42bd72fcf578b91fb02522057dca3037319680bb
Tomaz Muraus <tomaz@tomaz.me>
2011-10-29 17:37:57 +0200

    Start working on Rackspace DNS driver.

---
+++ libcloud/dns/drivers/rackspace.py
@@ -0,0 +1,224 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__all__ = [
+    'RackspaceUSDNSDriver'
+    'RackspaceUKDNSDriver'
+]
+
+from libcloud.common.base import AsyncConnection
+from libcloud.common.types import LibcloudError
+from libcloud.common.rackspace import AUTH_URL_US, AUTH_URL_UK
+from libcloud.compute.drivers.openstack import OpenStack_1_1_Connection
+from libcloud.compute.drivers.openstack import OpenStack_1_1_Response
+
+from libcloud.dns.types import Provider, RecordType
+from libcloud.dns.types import ZoneDoesNotExistError, RecordDoesNotExistError
+from libcloud.dns.base import DNSDriver, Zone, Record
+
+RECORD_TYPE_MAP = {
+    RecordType.A: 'A',
+    RecordType.AAAA: 'AAAA',
+    RecordType.CNAME: 'CNAME',
+    RecordType.MX: 'MX',
+    RecordType.NS: 'NS',
+    RecordType.TXT: 'TXT',
+    RecordType.SRV: 'SRV',
+}
+
+
+class RackspaceDNSResponse(OpenStack_1_1_Response):
+    """
+    Rackspace DNS Response class.
+    """
+
+    def parse_error(self):
+        # Holy fucking jesus,
+        # "The request could not be understood by the server due to malformed
+        # syntax." is returned if record already exists
+        status = int(self.status)
+        context = self.connection.context
+        body = self.parse_body()
+
+        if status == 404:
+            if context['resource'] == 'zone':
+                raise ZoneDoesNotExistError(value='', driver=self,
+                                            zone_id=context['id'])
+
+        if 'code' and 'message' in body:
+            err = '%s - %s (%s)' % (body['code'], body['message'],
+                                    body['details'])
+        elif 'validationErrors' in body:
+            errors = [m for m in body['validationErrors']['messages']]
+            err = 'Validation errors: %s' % ', '.join(errors)
+
+        return err
+
+
+class RackspaceDNSConnection(OpenStack_1_1_Connection, AsyncConnection):
+    """
+    Rackspace DNS Connection class.
+    """
+
+    responseCls = RackspaceDNSResponse
+    _url_key = 'dns_url'
+    XML_NAMESPACE = None
+
+    def get_poll_request_kwargs(self, response, context):
+        job_id = response.object['jobId']
+        kwargs = {'action': '/status/%s' % (job_id),
+                'params': {'showDetails': True}}
+        return kwargs
+
+    def has_completed(self, response):
+        status = response.object['status']
+        if status == 'ERROR':
+            raise LibcloudError(response.object['error']['message'],
+                                driver=self.driver)
+
+        return status == 'COMPLETED'
+
+
+class RackspaceUSDNSConnection(RackspaceDNSConnection):
+    auth_url = AUTH_URL_US
+
+
+class RackspaceUKDNSConnection(RackspaceDNSConnection):
+    auth_url = AUTH_URL_UK
+
+
+class RackspaceDNSDriver(DNSDriver):
+    def list_zones(self):
+        response = self.connection.request(action='/domains')
+        zones = self._to_zones(data=response.object['domains'])
+        return zones
+
+    def list_records(self, zone):
+        self.connection.set_context({'resource': 'zone', 'id': zone.id})
+        response = self.connection.request(action='/domains/%s' % (zone.id),
+                                           params={'showRecord': True}).object
+        records = self._to_records(data=response['recordsList']['records'],
+                                   zone=zone)
+        return records
+
+    def get_zone(self, zone_id):
+        self.connection.set_context({'resource': 'zone', 'id': zone_id})
+        response = self.connection.request(action='/domains/%s' % (zone_id))
+        zone = self._to_zone(data=response.object)
+        return zone
+
+    def create_zone(self, domain, type='master', ttl=None, extra=None):
+        extra = extra or {}
+
+        # Email address is required
+        if not 'email' in extra:
+            raise ValueError('"email" key must be present in extra dictionary')
+
+        payload = {'name': domain, 'emailAddress': extra['email'],
+                   'recordsList': {'records': []}}
+
+        if ttl:
+            payload['ttl'] = ttl
+
+        if 'comment' in extra:
+            payload['comment'] = extra['comment']
+
+        data = {'domains': [payload]}
+        response = self.connection.async_request(action='/domains',
+                                                 method='POST', data=data)
+        zone = self._to_zone(data=response.object['response']['domains'][0])
+        return zone
+
+    def create_record(self, name, zone, type, data, extra=None):
+        # Name must be a FQDN - e.g. if domain is "foo.com" then a record
+        # name is "bar.foo.com"
+        data = {'name': name, 'type': RECORD_TYPE_MAP[type], 'data': data}
+
+        if 'ttl' in extra:
+            data['ttl'] = int(extra['ttl'])
+
+        payload = {'records': [data]}
+        self.connection.set_context({'resource': 'zone', 'id': zone.id})
+        response = self.connection.async_request(action='/domains/%s/records'
+                                                 % (zone.id), data=payload,
+                                                 method='POST').object
+        record = self._to_record(data=response['response']['records'][0],
+                                 zone=zone)
+        return record
+
+    def delete_zone(self, zone):
+        self.connection.set_context({'resource': 'zone', 'id': zone.id})
+        self.connection.async_request(action='/domains/%s' % (zone.id),
+                                      method='DELETE')
+        return True
+
+    def _to_zones(self, data):
+        zones = []
+        for item in data:
+            zone = self._to_zone(data=item)
+            zones.append(zone)
+
+        return zones
+
+    def _to_zone(self, data):
+        id = data['id']
+        domain = data['name']
+        type = 'master'
+        ttl = data.get('ttl', 0)
+        extra = {}
+
+        if 'emailAddress' in data:
+            extra['email'] = data['emailAddress']
+
+        if 'comment' in data:
+            extra['comment'] = data['comment']
+
+        zone = Zone(id=str(id), domain=domain, type=type, ttl=int(ttl),
+                    driver=self, extra=extra)
+        return zone
+
+    def _to_records(self, data, zone):
+        records = []
+        for item in data:
+            record = self._to_record(data=item, zone=zone)
+            records.append(record)
+
+        return records
+
+    def _to_record(self, data, zone):
+        id = data['id']
+        name = data['name']
+        type = self._string_to_record_type(data['type'])
+        record_data = data['data']
+        extra = {}
+
+        if 'ttl' in data:
+            extra['ttl'] = data['ttl']
+
+        record = Record(id=str(id), name=name, type=type, data=record_data,
+                        zone=zone, driver=self, extra=extra)
+        return record
+
+
+class RackspaceUSDNSDriver(RackspaceDNSDriver):
+    name = 'Rackspace DNS (US)'
+    type = Provider.RACKSPACE_US
+    connectionCls = RackspaceUSDNSConnection
+
+
+class RackspaceUKDNSDriver(RackspaceDNSDriver):
+    name = 'Rackspace DNS (UK)'
+    type = Provider.RACKSPACE_UK
+    connectionCls = RackspaceUKDNSConnection
