0e11f5a3fff16d852b5fc236c30bb1ced8a6f698
Tomaz Muraus <tomaz@tomaz.me>
2011-10-30 00:40:19 +0200

    Add a docstring which explains how the function works.

--- libcloud/common/base.py
+++ libcloud/common/base.py
@@ -551,6 +551,46 @@ class PollingConnection(Connection):
 
     def async_request(self, action, params=None, data='', headers=None,
                       method='GET', context=None):
+        """
+        Perform an 'async' request to the specified path. Keep in mind that
+        this function is *blocking* and 'async' in this case means that the
+        hit URL only returns a job ID which is the periodically polled until 
+        the job has completed.
+
+        This function works like this:
+
+        - Perform a request to the specified path. Response should contain a
+          'job_id'.
+
+        - Returned 'job_id' is then used to construct a URL which is used for
+          retrieving job status. Constructed URL is then periodically polled
+          until the response indicates that the job has completed or the timeout
+          of 'self.timeout' seconds has been reached.
+
+        @type action: C{str}
+        @param action: A path
+
+        @type params: C{dict}
+        @param params: Optional mapping of additional parameters to send. If
+            None, leave as an empty C{dict}.
+
+        @type data: C{unicode}
+        @param data: A body of data to send with the request.
+
+        @type headers: C{dict}
+        @param headers: Extra headers to add to the request
+            None, leave as an empty C{dict}.
+
+        @type method: C{str}
+        @param method: An HTTP method such as "GET" or "POST".
+
+        @type context: C{dict}
+        @param context: Context dictionary which is passed to the functions
+        which construct initial and poll URL.
+
+        @return: An instance of type I{responseCls}
+        """
+
         request = getattr(self, self.request_method)
         kwargs = self.get_request_kwargs(action=action, params=params,
                                          data=data, headers=headers,
